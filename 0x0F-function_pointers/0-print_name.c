ce2b05ed98e9ea4665dda15a0ca36d27d8d5b59a
Ericmuturimwangi <muturiwamwanginanyawira@gmail.com>
2023-07-19 07:49:16 -0400

    pointer to function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name.
+ * @name: Pointer to the name to be printed.
+ * @f: Pointer to the function to be used for printing.
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
+/**
+ * print_name_as_is - Prints a name as is.
+ * @name: Pointer to the name to be printed.
+ *
+ * Return: void
+ */
+void print_name_as_is(char *name)
+{
+	printf("Hello, my name is %s\n", name);
+}
+
+/**
+ * print_name_uppercase - Prints a name in uppercase.
+ * @name: Pointer to the name to be printed.
+ *
+ * Return: void
+ */
+void print_name_uppercase(char *name)
+{
+	unsigned int i;
+
+	printf("Hello, my uppercase name is ");
+	i = 0;
+	while (name[i])
+	{
+		if (name[i] >= 'a' && name[i] <= 'z')
+		{
+			putchar(name[i] + 'A' - 'a');
+		}
+		else
+		{
+			putchar(name[i]);
+		}
+		i++;
+	}
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_name("Bob", print_name_as_is);
+	print_name("Bob Dylan", print_name_uppercase);
+	printf("\n");
+	return (0);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTION_PROTOTYPES_H
+#define FUNCTION_PROTOTYPES_H
+
+void print_name(char *name, void (*f)(char *));
+
+
+#endif /* FUNCTION_PROTOTYPES_H */
